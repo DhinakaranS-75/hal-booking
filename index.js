cce50bd58ac97f88d59d7f73c48ff71070e96d07
DhinakaranS-75 <dhinakaran75493@gmil.com>
2023-06-18 19:58:00 +0530

    update code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-// Define a local variable to store room data
+
 let rooms = [];
 
-// Endpoint to create a room
+
 app.post('/rooms', (req, res) => {
   const { seats, amenities, price } = req.body;
   
-  // Create a new room object
+  
   const room = {
     id: rooms.length + 1,
     seats,
@@ -17,24 +17,24 @@ app.post('/rooms', (req, res) => {
     bookings: []
   };
   
-  // Add the room to the rooms array
+  
   rooms.push(room);
   
   res.status(201).json(room);
 });
 
-// Endpoint to book a room
+
 app.post('/bookings', (req, res) => {
   const { customerName, date, startTime, endTime, roomId } = req.body;
   
-  // Find the room by its ID
+  
   const room = rooms.find(room => room.id === roomId);
   
   if (!room) {
     return res.status(404).json({ error: 'Room not found' });
   }
   
-  // Check if the room is already booked for the given date and time
+  
   const conflictingBooking = room.bookings.find(booking => (
     booking.date === date &&
     ((startTime >= booking.startTime && startTime < booking.endTime) ||
@@ -46,7 +46,7 @@ app.post('/bookings', (req, res) => {
     return res.status(400).json({ error: 'Room is already booked for the given date and time' });
   }
   
-  // Create a new booking object
+ 
   const booking = {
     id: room.bookings.length + 1,
     customerName,
@@ -55,13 +55,13 @@ app.post('/bookings', (req, res) => {
     endTime
   };
   
-  // Add the booking to the room's bookings array
+  
   room.bookings.push(booking);
   
   res.status(201).json(booking);
 });
 
-// Endpoint to list all rooms with booked data
+
 app.get('/rooms/bookings', (req, res) => {
   const bookedRooms = rooms.map(room => ({
     roomName: `Room ${room.id}`,
@@ -77,7 +77,7 @@ app.get('/rooms/bookings', (req, res) => {
   res.json(bookedRooms);
 });
 
-// Endpoint to list all bookings for a customer
+
 app.get('/customers/:customerName/bookings', (req, res) => {
   const customerName = req.params.customerName;
   
@@ -102,7 +102,7 @@ app.get('/customers/:customerName/bookings', (req, res) => {
   res.json(customerBookings);
 });
 
-// Start the server
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
